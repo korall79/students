7257ebf2b24a6c0fd6e97c5c0585bc6a4f266bf2
korall79 <koralll79@yandex.ru>
2023-02-25 18:48:49 +0400

    problem with DB driver solved

--- src/main/java/controllers/StudentsController.java
+++ src/main/java/controllers/StudentsController.java
@@ -16,6 +16,7 @@ public class StudentsController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //соед с бд и получ данных
         try {
+            Class.forName("com.mysql.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/students_3?user=root&password=Krilova1984/");
             Statement statement = connection.createStatement();
             ResultSet result = statement.executeQuery("select * from student;");
@@ -25,10 +26,9 @@ public class StudentsController extends HttpServlet {
                         + result.getString("name"));
                 System.out.println(text);
             }
-        } catch (SQLException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
         req.getRequestDispatcher("WEB-INF/jsp/students.jsp").forward(req,resp);
     }
 
