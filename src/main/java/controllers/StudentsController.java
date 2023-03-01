33a1b266471865cef4d2c705771bb998a406dea2
korall79 <koralll79@yandex.ru>
2023-03-01 20:36:50 +0400

    corrected students.jsp table, added dependency jstl, added DBManager class, corrected StudentsController

--- src/main/java/controllers/StudentsController.java
+++ src/main/java/controllers/StudentsController.java
@@ -1,5 +1,8 @@
 package controllers;
 
+import db.DBManager;
+import entity.Student;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.*;
+import java.util.List;
 
 
 @WebServlet(name = "StudentsController", urlPatterns = "/students")
@@ -14,21 +18,10 @@ public class StudentsController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //соед с бд и получ данных
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/students_3?user=root&password=Krilova1984/");
-            Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery("select * from student;");
 
-            while (result.next()){
-                String text = result.getInt("id")+ " "
-                        + result.getString("surname") + " " + result.getString("name");
-                System.out.println(text);
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        List<Student> students = DBManager.getAllActiveStudents();
+        req.setAttribute("students", students);
+
         req.getRequestDispatcher("WEB-INF/jsp/students.jsp").forward(req,resp);
     }
 
